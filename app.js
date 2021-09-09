13ef237fbe31a36cb2cf60407bf0ff54c81eae57
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-09 14:00:14 -0300

    Configurando Sessões e Msg de Middleware

--- app.js
+++ app.js
@@ -6,14 +6,33 @@ const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 //Configurações
-    //Body Parser
+//Sessão
+    app.use(session({
+        secret: "qualquercoisa",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+//Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
+//Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
+
 //Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars');
+
 //Mongoose
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/blogapp').then(() => {
@@ -21,8 +40,14 @@ const mongoose = require('mongoose')
     }).catch((err) => {
         console.log("Erro ao se conectar: "+err)
     })
+
 //Public
     app.use(express.static(path.join(__dirname,'public')))
+
+    app.use((req, res, next)=>{
+        console.log("OI EU SOU UM MIDWARE")
+        next()
+    })
 
 //Rotas
     app.get('/', (req, res) => {
