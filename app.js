d034a892ce0ea7a7150eb5184fc51af0b075ce3f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-12 17:20:00 -0300

    Página de Postagem

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ const Postagem = mongoose.model("postagens")
         })
     })
 
+    app.get("/postagem/:slug", (req, res) => {
+        Postagem.findOne({slug: req.params.slug}).then((postagem) => {
+            if(postagem){
+                res.render("postagem/index", {postagem: postagem.toJSON()})
+            }else {
+                req.flash("error_msg", "Está postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
     app.get("/404", (req, res) => {
         res.send("Erro 404!")
     })
